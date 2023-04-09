e0b5e997aaf903329abb701333f75162521c79f2
Mauricio Uribe <mandresuri@gmail.com>
2023-04-09 15:50:51 -0500

    multiple fixes

--- proyecto/admin.py
+++ proyecto/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from proyecto.models import *
+
 # Register your models here.
+
+admin.site.register(Servicio)
+admin.site.register(Producto)
+admin.site.register(Preventa)
+admin.site.register(Comercial)
+admin.site.register(PlanCapacidad)
--- proyecto/models.py
+++ proyecto/models.py
@@ -2,19 +2,24 @@ from django.db import models
 
 
 # Create your models here.
-class Servicios(models.Model):
+class Servicio(models.Model):
     nombre = models.CharField(max_length=50)
     descripcion = models.CharField(max_length=150, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f"{self.nombre}"
 
-class Productos(models.Model):
+
+class Producto(models.Model):
     nombre = models.CharField(max_length=50)
     descripcion = models.CharField(max_length=150, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f"{self.nombre}"
 
 class Preventa(models.Model):
     nombres = models.CharField(max_length=30)
@@ -22,7 +27,17 @@ class Preventa(models.Model):
     email = models.EmailField()
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return f"{self.nombres} {self.apellidos}"
+
+
+class Comercial(models.Model):
+    nombres = models.CharField(max_length=30)
+    apellidos = models.CharField(max_length=30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.nombres} {self.apellidos}"
+
 
 
 class PlanCapacidad(models.Model):
@@ -34,11 +49,12 @@ class PlanCapacidad(models.Model):
     dias_venta = models.IntegerField()
     prob_cierre = models.FloatField()
     ciudad_entrega = models.CharField(max_length=50)
-    account_manager = models.CharField(max_length=50)
+    comercial = models.ForeignKey(Comercial, on_delete=models.CASCADE, default=None)
     enlace_mat_cost = models.CharField(max_length=50)
     observaciones = models.CharField(max_length=150)
-    servicios = models.ManyToManyField(Servicios)
-    productos = models.ManyToManyField(Productos)
+    servicios = models.ManyToManyField(Servicio, blank=True)
+    productos = models.ManyToManyField(Producto, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
 
