b9950d5248a8981b40be93eb0b1da1874f92d004
Mauricio Uribe <mandresuri@gmail.com>
2023-04-09 11:39:46 -0500

    Django database config

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'projectos',
+        'USER': 'postgres',
+        'PASSWORD': 'admin1234',
+        'HOST': '127.0.0.1',
+        'DATABASE_PORT': '5432'
     }
 }
 
